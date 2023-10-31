a7283acb383e47666f8162de48d6d690d032f3d6
vitor2204 <vitorpeixotoc137@gmail.com>
2023-10-31 20:54:58 -0300

    initial styles login implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
 import Head from "next/head"
+import Image from "next/image"
+import styles from '../styles/home.module.scss'
+
+import { Input } from '../components/ui/Input'
+
+import logo2 from '../../public/logo1.png'
 
 export default function Home() {
   return (
@@ -7,7 +13,21 @@ export default function Home() {
         <title>Loreju - login</title>
       </Head>
       <div>
-        <h1>Loreju</h1>
+        <div className={styles.containerCenter}>
+          <Image src={logo2} alt="logo_loreju"/>
+          <div className={styles.login}>
+            <form>
+              <Input 
+                placeholder="digite seu email"
+                type='text'
+              />
+              <Input 
+                placeholder="digite sua senha"
+                type='password'
+              />
+            </form>
+          </div>
+        </div>
       </div>
     </>
   )
